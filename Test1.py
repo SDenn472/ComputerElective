b12910ac795c2731ebbdbcfd40a1f5739274f5cd
SDenn472 <17sdennis@iaa.edu.ec>
2015-05-08 08:42:48 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,10 @@
+operand1 = 95
+operand2 = 64.5 
+
+print "95 + 64.5 =",operand1 + operand2 
+print "95 - 64.5 =",operand1 - operand2
+print "95 * 64.5 =",operand1 * operand2
+print "95 / 64.5 =",operand1 / operand2
+print "95 % 64.5 =",operand1 % operand2
+
+# All I did was type a string and use the operand variables in place of the numbers and python figured it out on it's own.
