073277d2b83b59b1ff88a7879d90fe6423d1f8b0
SDenn472 <17sdennis@iaa.edu.ec>
2015-05-08 08:41:48 -0500

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,19 @@
+mystring = "hello"
+myint = 20
+myfloat = 10.0 
+
+print mystring
+print myint
+print myfloat
+
+print "My string is", mystring, "my integer is", myint, "my floating point is", myfloat
+print myint + myfloat 
+print myint - myfloat
+print myint * myfloat
+print myint / myfloat
+print myint % myfloat
+
+#Importing a new library
+import math
+root = math.sqrt(16)
+print root
