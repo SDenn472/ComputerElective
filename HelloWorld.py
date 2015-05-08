8189e06076ad67f156aa2784d86b79614f22ecd5
SDenn472 <17sdennis@iaa.edu.ec>
2015-05-08 08:39:29 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print "Hello World"
+print "This is Sarah. \nThis is Python"
+print 2015 #printing an interger
+print "2015" #printing a string 
+print 3.1416 #printing a floating point number 
+print 2 + 2 
+print 2 - 2 
+print 2 * 2 
+print 2 / 2 
+print 3.1416 * 2.0 
+print 3.1416 *2 
+print 3456.6678 / 10
+print 3 % 2 #modulus is the remainder of a 
+print 10 % 3 
